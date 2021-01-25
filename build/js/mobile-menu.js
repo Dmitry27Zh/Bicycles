0424f2b5c7e9abe0de04b4622146f0cf52a7955b
Dmitry <dmitry27zh@gmail.com>
2021-01-25 12:41:57 +0300

    добавлен скрипт работы мобильного меню

---
+++ build/js/mobile-menu.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var mainNavElement = document.querySelector('.main-nav');
+var toggleElement = mainNavElement.querySelector('.main-nav__toggle');
+
+mainNavElement.classList.remove('main-nav--no-js');
+
+toggleElement.addEventListener('click', function () {
+  if (!mainNavElement.classList.contains('main-nav--opened')) {
+    mainNavElement.classList.add('main-nav--opened');
+    document.body.classList.add('no-scroll');
+  } else {
+    mainNavElement.classList.remove('main-nav--opened');
+    document.body.classList.remove('no-scroll');
+  }
+});
