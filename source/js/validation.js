371104a45dc01ba590aa2aa565983687ec363d31
Dmitry <dmitry27zh@gmail.com>
2021-02-07 14:18:00 +0300

    добавлен скрипт валидации номера телефона

--- source/js/validation.js
+++ source/js/validation.js
@@ -1,27 +1,6 @@
 'use strict';
 
 (function () {
-  var mainNavElement = document.querySelector('.main-nav');
-  var toggleElement = mainNavElement.querySelector('.main-nav__toggle');
-
-  if (mainNavElement && toggleElement) {
-    mainNavElement.classList.remove('main-nav--no-js');
-
-    toggleElement.addEventListener('click', function () {
-      if (!mainNavElement.classList.contains('main-nav--opened')) {
-        mainNavElement.classList.add('main-nav--opened');
-        document.body.classList.add('no-scroll');
-      } else {
-        mainNavElement.classList.remove('main-nav--opened');
-        document.body.classList.remove('no-scroll');
-      }
-    });
-  }
-})();
-
-'use strict';
-
-(function () {
   var callbackForm = document.querySelector('.promo__callback');
   var inputTel = callbackForm.querySelector('input[type="tel"');
   var pattern = '^([+]7|8)[0-9]{10}$';
